5c123019ba738220a7bb85542ede8556c1bd7542
suil0304 <eagle030410@gmail.com>
2026-07-09 13:21:57 +0900

    solved data-management-using-sql/2/column-change-practice

---
+++ data-management-using-sql/2/column-change-practice.sql
@@ -0,0 +1,10 @@
+-- 06. 컬럼 구조 변경 실습 정답
+
+# 여기에 코드를 작성하세요
+ALTER TABLE `shoes`
+    MODIFY `size` DOUBLE,
+    DROP COLUMN `brand`,
+    ADD COLUMN `stock` INT NOT NULL,
+    RENAME COLUMN `name` TO `model`;
+
+DESCRIBE `shoes`
