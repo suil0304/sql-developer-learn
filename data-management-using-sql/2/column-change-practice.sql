afc19bb70916faa14d8998bf2a885c13fd480664
suil0304 <eagle030410@gmail.com>
2026-07-09 13:23:48 +0900

    Add semicolon

--- data-management-using-sql/2/column-change-practice.sql
+++ data-management-using-sql/2/column-change-practice.sql
@@ -2,9 +2,9 @@
 
 # 여기에 코드를 작성하세요
 ALTER TABLE `shoes`
-    MODIFY `size` DOUBLE,
+    MODIFY `size` DOUBLE NOT NULL,
     DROP COLUMN `brand`,
     ADD COLUMN `stock` INT NOT NULL,
     RENAME COLUMN `name` TO `model`;
 
-DESCRIBE `shoes`
+DESCRIBE `shoes`;
